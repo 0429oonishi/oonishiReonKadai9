a7a95824906ab6e1844a564193916d52671629f9
0429oonishi <oonishireon110@gmail.com>
2021-07-10 23:50:05 +0900

    prefectureにid追加

--- oonishiReonKadai9/Models/Prefecture.swift
+++ oonishiReonKadai9/Models/Prefecture.swift
@@ -9,11 +9,12 @@ import Foundation
 
 struct Prefecture {
     let name: String
+    let id: Int
 }
 
 extension Prefecture {
-    static let data = [Prefecture(name: "東京都"),
-                       Prefecture(name: "神奈川県"),
-                       Prefecture(name: "埼玉県"),
-                       Prefecture(name: "千葉県")]
+    static let data = [Prefecture(name: "東京都", id: 0),
+                       Prefecture(name: "神奈川県", id: 1),
+                       Prefecture(name: "埼玉県", id: 2),
+                       Prefecture(name: "千葉県", id: 3)]
 }
