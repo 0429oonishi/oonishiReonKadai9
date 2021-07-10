c4dac6b6be847a9319d2b229c1b448f4103fc37c
0429oonishi <oonishireon110@gmail.com>
2021-07-10 22:56:34 +0900

    PrefecturesVC作成

---
+++ oonishiReonKadai9/PrefecturesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PrefecturesViewController.swift
+//  oonishiReonKadai9
+//
+//  Created by 大西玲音 on 2021/07/10.
+//
+
+import UIKit
+
+class PrefecturesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
