ced8a68d6a7ca91ef6202afa3b05e8f5bd385762
0429oonishi <oonishireon110@gmail.com>
2021-07-14 19:35:32 +0900

    PrefecturesViewModel実装

--- oonishiReonKadai9/ViewModels/PrefecturesViewModel.swift
+++ oonishiReonKadai9/ViewModels/PrefecturesViewModel.swift
@@ -5,4 +5,35 @@
 //  Created by 大西玲音 on 2021/07/14.
 //
 
-import Foundation
+import RxSwift
+import RxCocoa
+
+protocol PrefecturesViewModelInput {
+    
+}
+
+protocol PrefecturesViewModelOutput: AnyObject {
+    
+}
+
+protocol PrefecturesViewModelType {
+    var inputs: PrefecturesViewModelInput { get }
+    var outputs: PrefecturesViewModelOutput { get }
+}
+
+final class PrefecturesViewModel: PrefecturesViewModelInput,
+                                  PrefecturesViewModelOutput {
+    
+}
+
+extension PrefecturesViewModel: PrefecturesViewModelType {
+    
+    var inputs: PrefecturesViewModelInput {
+        return self
+    }
+    
+    var outputs: PrefecturesViewModelOutput {
+        return self
+    }
+    
+}
