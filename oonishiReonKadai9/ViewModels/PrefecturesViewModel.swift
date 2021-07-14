e5296410ae9214d1bf71304144e3ccde889cd0d2
0429oonishi <oonishireon110@gmail.com>
2021-07-14 20:27:18 +0900

    PrefecturesVMリファクタリング

--- oonishiReonKadai9/ViewModels/PrefecturesViewModel.swift
+++ oonishiReonKadai9/ViewModels/PrefecturesViewModel.swift
@@ -9,11 +9,13 @@ import RxSwift
 import RxCocoa
 
 protocol PrefecturesViewModelInput {
-    
+    func cancelButtonDidTapped()
+    func prefectureButtonDidTapped(index: Int)
 }
 
 protocol PrefecturesViewModelOutput: AnyObject {
-    
+    var prefectures: [Prefecture] { get }
+    var event: Driver<PrefecturesViewModel.Event> { get }
 }
 
 protocol PrefecturesViewModelType {
@@ -23,6 +25,27 @@ protocol PrefecturesViewModelType {
 
 final class PrefecturesViewModel: PrefecturesViewModelInput,
                                   PrefecturesViewModelOutput {
+    enum Event {
+        case dismiss
+        case returnSelectedName(String)
+    }
+
+    let prefectures: [Prefecture] = Prefecture.data
+    
+    private let eventRelay = PublishRelay<Event>()
+    var event: Driver<Event> {
+        eventRelay.asDriver(onErrorDriveWith: .empty())
+    }
+    
+    func cancelButtonDidTapped() {
+        eventRelay.accept(.dismiss)
+    }
+    
+    func prefectureButtonDidTapped(index: Int) {
+        let name = prefectures[index].name
+        eventRelay.accept(.returnSelectedName(name))
+        eventRelay.accept(.dismiss)
+    }
     
 }
 
